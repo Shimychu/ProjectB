ec197781ec266e763ab377868221a2bfee70fa89
Shimychu <phenizer@hotmail.com>
2020-10-19 15:24:20 -0700

    Initial Commit

---
+++ Source/ProjectB/ProjectB.cpp
@@ -0,0 +1,7 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "ProjectB.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, ProjectB, "ProjectB" );
+ 
